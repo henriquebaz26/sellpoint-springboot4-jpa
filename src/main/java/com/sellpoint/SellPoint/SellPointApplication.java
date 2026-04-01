7ee76b0eb15ff329991e9a2cbd86dec2c9d19a7d
henriquebaz26 <h24henriquebaz@gmail.com>
2026-04-01 08:01:32 -0300

    Project created

---
+++ src/main/java/com/sellpoint/SellPoint/SellPointApplication.java
@@ -0,0 +1,13 @@
+package com.sellpoint.SellPoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellPointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sellpoint/SellPoint/SellPointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sellpoint.SellPoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellPointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

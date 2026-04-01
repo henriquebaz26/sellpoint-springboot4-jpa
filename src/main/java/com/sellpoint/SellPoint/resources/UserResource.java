0061d0cf43e4d9a48e62633d38f68834fe164dbe
henriquebaz26 <h24henriquebaz@gmail.com>
2026-04-01 10:02:25 -0300

    Service layer, component registration

--- src/main/java/com/sellpoint/SellPoint/resources/UserResource.java
+++ src/main/java/com/sellpoint/SellPoint/resources/UserResource.java
@@ -1,22 +1,36 @@
 package com.sellpoint.SellPoint.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.sellpoint.SellPoint.entities.User;
+import com.sellpoint.SellPoint.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
+    public ResponseEntity<List<User>> findAll() {
 
-        User u = new User(1L, "Henrique Baz", "henrique@gmail.com", "9999-9999", "senha123");
+        List<User> users = service.findAll();
 
-        return ResponseEntity.ok().body(u);
+        return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
     }
 
 }

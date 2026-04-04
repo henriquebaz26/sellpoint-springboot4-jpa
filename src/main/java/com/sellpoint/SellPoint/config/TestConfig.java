9594f63d3b47652e0637f348023a6c4997a3bbd1
henriquebaz26 <h24henriquebaz@gmail.com>
2026-04-04 11:17:39 -0300

    Payment, one-to-one association

--- src/main/java/com/sellpoint/SellPoint/config/TestConfig.java
+++ src/main/java/com/sellpoint/SellPoint/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.sellpoint.SellPoint.entities.Category;
 import com.sellpoint.SellPoint.entities.Order;
 import com.sellpoint.SellPoint.entities.OrderItem;
+import com.sellpoint.SellPoint.entities.Payment;
 import com.sellpoint.SellPoint.entities.Product;
 import com.sellpoint.SellPoint.entities.User;
 import com.sellpoint.SellPoint.entities.OrderEnums.OrderStatus;
@@ -82,6 +83,12 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
 
     }
 

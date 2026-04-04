2083cf2d6cd5e2bacd031c486cd25d558d89575f
henriquebaz26 <h24henriquebaz@gmail.com>
2026-04-04 16:27:28 -0300

    Exception handling - delete

---
+++ src/main/java/com/sellpoint/SellPoint/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.sellpoint.SellPoint.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+
+}

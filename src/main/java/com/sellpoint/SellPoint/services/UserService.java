21c4ec5f5dc6d2dab517a1fbaade90bc75e4fa8f
henriquebaz26 <h24henriquebaz@gmail.com>
2026-04-04 15:48:15 -0300

    Exception handling - findById

--- src/main/java/com/sellpoint/SellPoint/services/UserService.java
+++ src/main/java/com/sellpoint/SellPoint/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.sellpoint.SellPoint.entities.User;
 import com.sellpoint.SellPoint.repositories.UserRepository;
+import com.sellpoint.SellPoint.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
     public User findById(Long id) {
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj) {
